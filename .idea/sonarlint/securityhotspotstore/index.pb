6554e38acda7e8211cc66fbbff192e1959fc0ed8
Tobi <tobias_zoppi@hotmail.com>
2023-04-24 20:09:19 -0300

    Lista para produccion.

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,5 @@
 
-q
-Asrc/main/java/com/tobiaszoppi/metabolom/MetabolomApplication.java,a\4\a40190de1bceb47f6ce76b4d3d56584dc3be705d
+w
+G.idea/sonarlint/issuestore/a/4/a40190de1bceb47f6ce76b4d3d56584dc3be705d,7\3\737bf5ae2d70c1d110ce83bbf9873aaa57d2c9f7
+Å
+Q.idea/sonarlint/securityhotspotstore/a/4/a40190de1bceb47f6ce76b4d3d56584dc3be705d,b\f\bfe7043111d4f6b68afbda2c9aacffe5a66b2e93
--- src/ui/src/App.js
+++ src/ui/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './pages/Layout'
+import Home from './pages/Home'
+import Customers from './pages/Customers'
+import Products from './pages/Products'
+import Services from './pages/Services'
+import Vendors from './pages/Vendors'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path={'/'} element={<Layout/>}>
+              <Route index element={<Home/>}/>
+              <Route path={'customers'} element={<Customers/>}/>
+              <Route path={'products'} element={<Products/>}/>
+              <Route path={'services'} element={<Services/>}/>
+              <Route path={'vendors'} element={<Vendors/>}/>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
   );
 }
 
