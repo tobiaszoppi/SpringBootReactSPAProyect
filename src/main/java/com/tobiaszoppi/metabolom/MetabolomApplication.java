9ed10912002a6d729f6d25af5ea00383c81d4dfe
Tobi <tobias_zoppi@hotmail.com>
2023-04-24 20:55:03 -0300

    Agregando React

---
+++ src/main/java/com/tobiaszoppi/metabolom/MetabolomApplication.java
@@ -0,0 +1,13 @@
+package com.tobiaszoppi.metabolom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetabolomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetabolomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tobiaszoppi/metabolom/MetabolomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobiaszoppi.metabolom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetabolomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
